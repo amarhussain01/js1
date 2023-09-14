3da704412717d14af693efc10470cc19c3179931
Amar Hussain <99237742+amarhussain01@users.noreply.github.com>
2023-09-14 01:44:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("I Like Pizza");
+console.log("It's really good!");
+
+window.alert("I Really Love Pizza");
+
+//This is a comment
+
+/*
+This
+is
+a 
+mulitiline
+comment
+*/
+
+let firstName = " Bro"; //strings
+let age = 21; //number
+let student = true; //booleans
+
+console.log("Hello", firstName);
+console.log("You are", age, "years old");
+console.log("Enrolled", student);
+
+document.getElementById("p1").innerHTML = "Hello" + firstName;
+document.getElementById("p2").innerHTML = "You are "  + age + " years old";
+document.getElementById("p3").innerHTML = "Enrolled: " + student;
